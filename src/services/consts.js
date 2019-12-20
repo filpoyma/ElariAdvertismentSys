10e007269efca4768bfc28fcfeb4e013f1213669
rparamzin <rparamzin@elari.net>
2019-12-20 17:02:59 +0300

    add chromeStart service

--- src/services/consts.js
+++ src/services/consts.js
@@ -1,2 +1,2 @@
-export const API_BASE_ADS = "https://ads.elari.systems/madss_api.php";
-export const API_BASE_LOG = 'https://sdk.appclick.org/js-ads/log';
+export const API_BASE_ADS = "http://ads.elari.systems/madss_api.php";
+export const API_BASE_LOG = 'http://sdk.appclick.org/js-ads/log';
